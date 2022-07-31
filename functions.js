ac141a007ee05b85967cb8d3600d89eb7d1c5363
Anuradha <109025058+anuradha0910@users.noreply.github.com>
2022-07-31 18:34:26 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+  function alpha(str) {
+    var arr = str.split(""); 
+    res = arr.sort().join(""); 
+    return res; 
+  }
+  str =prompt("enter any string")
+  document.write(alpha(str));
+</script>
+</body>
+</html>
---
+++ json.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p id="demo"></p>
+
+<script>
+const simpleArrayJson = ["Toyota Rav4"];
+const objArrayJson = JSON.parse = ('{"name":"Toyota Rav4", engine:"2.5L", price: 32000}');
+
+document.write(simpleArrayJson);
+document.write(objArrayJson);
+
+</script>
+</body>
+</html>
